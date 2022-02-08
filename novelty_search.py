d830050bc346796c96f1e078acc6edd23eb728df
Eyal <eyalseg@post.bgu.ac.il>
2022-02-08 17:19:53 +0200

    extracted ns args to cmdline

--- novelty_search.py
+++ novelty_search.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from functools import partial
 
 import toolz
@@ -27,13 +28,25 @@ def last_observation_behaviour_characteristic(env, agent):
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument("--env", type=str)
+    parser.add_argument("--popsize", type=int)
+    parser.add_argument("--validation_episodes", type=int)
+    parser.add_argument("--fitness_robustness", type=int)
+    parser.add_argument("--mutation_strength", type=float)
+    parser.add_argument("--truncation_size", type=int)
+    parser.add_argument("--train_steps", type=int, default=int(1e6))
+    parser.add_argument("--novelty_neighbors", type=int)
+    parser.add_argument("--archive_pr", type=float)
+
+    args = parser.parse_args()
+
     logger = ConsoleLogger()
 
-    args = {}
-
-    trainer = Trainer(env_name="Acrobot-v1",
-                      max_train_steps=int(1e6),
-                      validation_episodes=100,
+    trainer = Trainer(env_name=args.env,
+                      max_train_steps=args.train_steps,
+                      validation_episodes=args.validation_episodes,
                       logger=logger)
 
     policy_dims = [sum(trainer.train_env.observation_space.shape),
@@ -43,19 +56,19 @@ if __name__ == "__main__":
 
     initializer = partial(toolz.compose_left(LinearTorchPolicy, TorchPolicyAgent), policy_dims)
     rollout = last_observation_behaviour_characteristic(trainer.train_env)
-    mutator = add_gaussian_noise(0.8)
-    selector = truncated_selection(6)
-    novelty_measure = knn_novelty(40)
+    mutator = add_gaussian_noise(args.mutation_strength)
+    selector = truncated_selection(args.truncation_size)
+    novelty_measure = knn_novelty(args.novelty_neighbors)
 
     ns = NoveltySearch(
-        pop_size=50,
+        pop_size=args.popsize,
         initializer=initializer,
         rollout=rollout,
         novelty_measure=novelty_measure,
-        archive_pr=0.1,
+        archive_pr=args.archive_pr,
         survivors_selector=selector,
         mutator=mutator,
-        robustness=5
+        robustness=args.fitness_robustness,
     )
     trainer.fit(ns)
 
