21289c26f49a87af3750d478298d68232ca612b9
Eyal <eyalseg@post.bgu.ac.il>
2022-03-07 15:40:38 +0200

    added types to selection.py

--- algorithms/operators/selection.py
+++ algorithms/operators/selection.py
@@ -1,8 +1,12 @@
+from typing import List
+
 import toolz
+
+from agents.agent_typing import Agent
 
 
 @toolz.curry
-def truncated_selection(truncation_len, population, fitnesses):
+def truncated_selection(truncation_len: int, population: List[Agent], fitnesses: List[Agent]):
     fit_pop = [{"specimen": specimen, "fitness": fit} for specimen, fit in zip(population, fitnesses)]
     selected = list(toolz.topk(truncation_len, fit_pop, key=lambda x: x["fitness"]))
 
