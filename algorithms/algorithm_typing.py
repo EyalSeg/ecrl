bb3f415276d38825e5b4e80c61dea29bf0c21852
Eyal <eyalseg@post.bgu.ac.il>
2022-03-07 11:48:15 +0200

    added more types

--- algorithms/algorithm_typing.py
+++ algorithms/algorithm_typing.py
@@ -1,5 +1,8 @@
-from typing import Protocol
+from numpy import array
 
+from dataclasses import dataclass
+
+from typing import Protocol, Callable, TypeVar, List
 from agents.agent_typing import Agent
 
 
@@ -8,3 +11,22 @@ class EvolutionaryAlgorithm(Protocol):
 
     elite: Agent
     elite_fitness: float
+
+
+@dataclass
+class Trajectory:
+    observations: array
+    actions: array
+    rewards: array
+
+
+BC = TypeVar('BC')
+
+Initializer = Callable[[], Agent]
+Rollout = Callable[[Agent], Trajectory]
+Fitness = Callable[[BC], float]
+BehaviorCharacteristic = Callable[[Trajectory], BC]
+SurvivorSelector = Callable[[List[Agent], List[float]], List[Agent]]
+Mutator = Callable[[Agent], None]
+
+
