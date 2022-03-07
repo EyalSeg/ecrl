865754be6dd93432983eb1fe241463df47591b37
Eyal <eyalseg@post.bgu.ac.il>
2022-03-07 15:48:16 +0200

    added elite extraction

--- algorithms/algorithm_typing.py
+++ algorithms/algorithm_typing.py
@@ -2,15 +2,8 @@ from numpy import array
 
 from dataclasses import dataclass
 
-from typing import Protocol, Callable, TypeVar, List
+from typing import Protocol, Callable, TypeVar, List, Generic, Tuple
 from agents.agent_typing import Agent
-
-
-class EvolutionaryAlgorithm(Protocol):
-    def generation(self): ...
-
-    elite: Agent
-    elite_fitness: float
 
 
 @dataclass
@@ -24,9 +17,23 @@ BC = TypeVar('BC')
 
 Initializer = Callable[[], Agent]
 Rollout = Callable[[Agent], Trajectory]
-Fitness = Callable[[BC], float]
+FitnessMeasure = Callable[[BC], float]
+BatchFitnessMeasure = Callable[[List[BC]], List[float]]
+ArchiveToBatchFitness = Callable[[List[BC]], BatchFitnessMeasure]
 BehaviorCharacteristic = Callable[[Trajectory], BC]
 SurvivorSelector = Callable[[List[Agent], List[float]], List[Agent]]
 Mutator = Callable[[Agent], None]
+EliteExtractor = Callable[[List[Agent], List[float]], Tuple[Agent, float]]
 
+
+class EvolutionaryAlgorithm(Protocol):
+    def generation(self): ...
+
+    elite: Agent
+    elite_fitness: float
+
+
+class Archive(Protocol[BC]):
+    def store(self, item: List[BC]) -> None: ...
+    def retrieve(self) -> List[BC]: ...
 
--- algorithms/operators/selection.py
+++ algorithms/operators/selection.py
@@ -1,8 +1,10 @@
 from typing import List
+from numpy import argmax
 
 import toolz
 
 from agents.agent_typing import Agent
+from algorithms.algorithm_typing import FitnessMeasure
 
 
 @toolz.curry
@@ -11,4 +13,17 @@ def truncated_selection(truncation_len: int, population: List[Agent], fitnesses:
     selected = list(toolz.topk(truncation_len, fit_pop, key=lambda x: x["fitness"]))
 
     return selected
+
+@toolz.curry
+def find_true_elite(candidates_len: int, fitness: FitnessMeasure, robustness: int,
+                    population: List[Agent], fitnesses: List[Agent]) -> (Agent, float):
+
+    candidates = truncated_selection(candidates_len, population, fitnesses)
+    agent_episodes = [[fitness(agent) for _ in range(robustness)] for agent in population]
+    fitnesses = [sum(episodes) / len(episodes) for episodes in agent_episodes]
+
+    elite_idx = argmax(fitnesses)
+
+    return candidates[elite_idx], fitnesses[elite_idx]
+
 
