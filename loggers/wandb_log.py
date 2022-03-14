63f579a4e7a070c02aae326c2dc1813da38e9415
Eyal <eyalseg@post.bgu.ac.il>
2022-03-14 15:29:51 +0200

    added group to wandb logger

--- loggers/wandb_log.py
+++ loggers/wandb_log.py
@@ -2,8 +2,8 @@ import wandb
 
 
 class WandbLogger:
-    def __init__(self, project, entity, config):
-        wandb.init(project=project, entity=entity, config=config)
+    def __init__(self, project, entity, config, group=None):
+        wandb.init(project=project, entity=entity, config=config, group=group)
 
     def log(self, metrics):
         wandb.log(metrics)
