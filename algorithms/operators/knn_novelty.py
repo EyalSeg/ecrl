2cccec2779b9f1040886c8aacaf02e44ca1893a7
Eyal <eyalseg@post.bgu.ac.il>
2022-03-31 17:32:16 +0300

    changed knn novelty to standardize instead of normalize

--- algorithms/operators/knn_novelty.py
+++ algorithms/operators/knn_novelty.py
@@ -3,7 +3,7 @@ from typing import List
 import toolz
 import numpy as np
 
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 
 from algorithms.algorithm_typing import Archive, BatchFitnessMeasure
@@ -15,12 +15,12 @@ def archive_to_knn_novelty(k: int, archive: Archive[np.array]) -> BatchFitnessMe
         combined = archive.retrieve() + batch
         combined = np.row_stack(combined)
 
-        combined_normalized = normalize(combined, axis=0, norm="max")
+        combined_standardized = StandardScaler().fit_transform(combined)
 
         # each element in the batch is stored in the knn,
         # we add 1 to retrieve it and ignore it (it is 0 distance)
         knn = NearestNeighbors(n_neighbors=k+1)
-        knn.fit(combined_normalized)
+        knn.fit(combined_standardized)
 
         distances, indices = knn.kneighbors(batch)
         avg = np.sum(distances, axis=1) / k
