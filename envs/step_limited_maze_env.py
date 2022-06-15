e906c16442bc874447a84532fa6c4c3ff40ce14c
eyal <eyalseg@post.bgu.ac.il>
2022-06-15 16:15:03 +0300

    refactored time limited maze env

--- envs/step_limited_maze_env.py
+++ envs/step_limited_maze_env.py
@@ -1,16 +1,16 @@
 from mujoco_maze.maze_env import MazeEnv
 
 
-class TimeLimitedMazeEnv(MazeEnv):
+class StepLimitedMazeEnv(MazeEnv):
     def __init__(self, time_limit, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.time_limit = time_limit
+        self._max_episode_steps = time_limit
 
     def step(self, action):
         next_obs, reward, done, info = super().step(action)
 
-        if self.t >= self.time_limit:
+        if self.t >= self._max_episode_steps:
             done = True
 
         return next_obs, reward, done, info
