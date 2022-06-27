28b40a2aa3e376461848d65505ef3c195aaecc1d
eyal <eyalseg@post.bgu.ac.il>
2022-06-27 17:06:43 +0300

    added sparse small maze

--- envs/__init__.py
+++ envs/__init__.py
@@ -2,7 +2,7 @@ import gym
 from mujoco_maze import PointEnv, AntEnv
 
 from envs.large_maze_deceptive import LargeMazeDeceptive
-from envs.small_maze_deceptive import SmallMazeDeceptive
+from envs.small_maze_deceptive import SmallMazeDeceptive, SmallMazeSparse
 
 gym.envs.register(
     id="PointMazeSmallDeceptive-v0",
@@ -17,12 +17,36 @@ gym.envs.register(
 )
 
 gym.envs.register(
+    id="PointMazeSmallSparse-v0",
+    entry_point="envs.step_limited_maze_env:StepLimitedMazeEnv",
+    kwargs=dict(
+        model_cls=PointEnv,
+        maze_task=SmallMazeSparse,
+        maze_size_scaling=SmallMazeSparse.MAZE_SIZE_SCALING.point,
+        inner_reward_scaling=0,
+        time_limit=500,
+    )
+)
+
+gym.envs.register(
     id="AntMazeSmallDeceptive-v0",
     entry_point="envs.step_limited_maze_env:StepLimitedMazeEnv",
     kwargs=dict(
         model_cls=AntEnv,
         maze_task=SmallMazeDeceptive,
         maze_size_scaling=SmallMazeDeceptive.MAZE_SIZE_SCALING.ant,
+        inner_reward_scaling=0,
+        time_limit=750,
+    )
+)
+
+gym.envs.register(
+    id="AntMazeSmallSparse-v0",
+    entry_point="envs.step_limited_maze_env:StepLimitedMazeEnv",
+    kwargs=dict(
+        model_cls=AntEnv,
+        maze_task=SmallMazeSparse,
+        maze_size_scaling=SmallMazeSparse.MAZE_SIZE_SCALING.ant,
         inner_reward_scaling=0,
         time_limit=750,
     )
--- envs/small_maze_deceptive.py
+++ envs/small_maze_deceptive.py
@@ -2,6 +2,20 @@ import numpy as np
 from mujoco_maze.maze_env_utils import MazeCell
 from mujoco_maze.maze_task import MazeTask, MazeGoal
 
+o, I, R = MazeCell.EMPTY, MazeCell.BLOCK, MazeCell.ROBOT,
+MAZE = [
+    [I, I, I, I, I, I, I],
+    [I, o, o, o, o, o, I],
+    [I, o, o, o, o, o, I],
+    [I, o, o, o, o, o, I],
+    [I, o, o, o, o, o, I],
+    [I, o, I, I, I, o, I],
+    [I, o, I, o, I, o, I],
+    [I, o, I, R, I, o, I],
+    [I, o, o, o, o, o, I],
+    [I, I, I, I, I, I, I],
+]
+
 
 class SmallMazeDeceptive(MazeTask):
     REWARD_THRESHOLD: float = 0
@@ -21,17 +35,20 @@ class SmallMazeDeceptive(MazeTask):
 
     @staticmethod
     def create_maze():
-        o, I, R = MazeCell.EMPTY, MazeCell.BLOCK, MazeCell.ROBOT,
-        return [
-            [I, I, I, I, I, I, I],
-            [I, o, o, o, o, o, I],
-            [I, o, o, o, o, o, I],
-            [I, o, o, o, o, o, I],
-            [I, o, o, o, o, o, I],
-            [I, o, I, I, I, o, I],
-            [I, o, I, o, I, o, I],
-            [I, o, I, R, I, o, I],
-            [I, o, o, o, o, o, I],
-            [I, I, I, I, I, I, I],
+       return MAZE
 
-        ]
+
+class SmallMazeSparse(MazeTask):
+    REWARD_THRESHOLD: float = 0
+    PENALTY: float = 0
+
+    def __init__(self, scale):
+        super().__init__(scale)
+        self.goals = [MazeGoal(np.array([0.0, -6.0]) * scale, threshold=1)]
+
+    def reward(self, obs):
+        return 0 if self.termination(obs) else -1
+
+    @staticmethod
+    def create_maze():
+        return MAZE
